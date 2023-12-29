9192cf9138f42118389a8f82c0a56a08c93360cc
Rajdip27 <srajdip920"gmail.com>
2023-12-29 23:56:28 +0600

    add project

---
+++ OOPCShrap/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace OOPCShrap;
+
+public class Person
+{
+    public string fristName;
+    public string middleName;
+    public string lastName;
+    public string GetFullName() => $"{fristName} {middleName} {lastName}";
+}
+
+//object is a real world entity.for example Chair,car,pen Mobile,Laptop ctc.
+//object is an entity that has state and behavior. here ,state means data and behavior means functionality.
---
+++ OOPCShrap/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+using OOPCShrap;
+
+Person person = new Person();
+person.fristName = "Rajdip";
+person.lastName = "Raj";
+person.middleName = "Santanu";
+Console.WriteLine(person.GetFullName());
+Console.ReadKey();
+
