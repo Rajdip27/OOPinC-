e3f30bde9e423e4176ba61c3b83e984ba1cb04d0
Rajdip27 <Srajdip920@gmail.com>
2024-01-14 11:55:19 +0600

    AddExtenstionMethod

---
+++ ExtenstionMethod/Extenstion.cs
@@ -0,0 +1,5 @@
+﻿namespace ExtenstionMethod;
+public static class Extenstion
+{
+    public static string PersonalInfo(this Person person)=> $"Id: {person.Id},Name:{person.Name},Address :{person.Address}";
+}
---
+++ ExtenstionMethod/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using ExtenstionMethod;
+
+Person Person= new Person();
+Person.Id=1;
+Person.Name = "Abcd";
+Person.Address = "jkndfjkn";
+string personalInfo = Person.PersonalInfo();
+Console.WriteLine(personalInfo);
+Console.ReadKey();
