77a03d8aac818615ad80011e82ac0b309f770517
Rajdip27 <Srajdip920@gmail.com>
2024-01-15 22:57:46 +0600

    Single delegates

---
+++ SingleDelegates/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public delegate int Sumation(int firstNumber, int secondNumber);
+
+class Program
+{
+    static void Main()
+    {
+        //Sumation sumation = new Sumation(Sum);
+        Sumation sumation = Sum;
+       //int result= sumation.Invoke(12, 23);
+       int result= sumation(12, 23);
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+
+    static int Sum(int firstNumber, int secondNumber)=> firstNumber+ secondNumber;
+    
+}
---
+++ SingleDelegates/Student.cs
@@ -0,0 +1,7 @@
+﻿namespace SingleDelegates;
+public  class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
